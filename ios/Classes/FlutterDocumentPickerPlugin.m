36b06f17478c2218f84724d7da47acc2105ce1c4
Mykhailo <mish_k98@mail.ru>
2019-05-20 17:37:46 +0300

    smth

--- ios/Classes/FlutterDocumentPickerPlugin.m
+++ ios/Classes/FlutterDocumentPickerPlugin.m
@@ -1,8 +1,8 @@
 #import "FlutterDocumentPickerPlugin.h"
-#import <flutter_document_picker/flutter_document_picker-Swift.h>
+//#import <flutter_document_picker/flutter_document_picker-Swift.h>
 
 @implementation FlutterDocumentPickerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterDocumentPickerPlugin registerWithRegistrar:registrar];
+  [FlutterDocumentPickerPlugin registerWithRegistrar:registrar];
 }
 @end
